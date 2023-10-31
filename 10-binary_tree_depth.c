bd074edf4f767ea5f34c83880695f62c3ffa8c6d
markrizkalla <mark.rizkalla1@gmail.com>
2023-10-31 13:15:56 -0700

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,7 +10,7 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree->parent == NULL || tree == NULL)
-		return 0;
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
 	return (binary_tree_depth(tree->parent) + 1);
 }
