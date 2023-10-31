f24d8c8408f9a347dc6416a6862cd7b1390a8206
markrizkalla <mark.rizkalla1@gmail.com>
2023-10-31 16:45:56 -0700

    18-uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -19,5 +19,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		       node->parent->parent->right == node->parent)
 		return (node->parent->parent->left);
 
-	return node->parent->parent->right;
+	return (node->parent->parent->right);
 }
