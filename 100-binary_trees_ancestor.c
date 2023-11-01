0a83730ae698e64a8f04af65fa080460e21b22f4
markrizkalla <mark.rizkalla1@gmail.com>
2023-11-01 10:42:54 -0700

    100-ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,76 +1,55 @@
 #include "binary_trees.h"
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+/**
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+ *
+ * @first: A pointer to the first node.
+ * @second: A pointer to the second node.
+ *
+ * Return: A pointer to the lowest common ancestor node of the two given nodes.
+ *         If no common ancestor was found, your function must return NULL.
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
 	if (first == NULL || second == NULL)
 		return (NULL);
 
 	const binary_tree_t *node1 = first;
-
 	const binary_tree_t *node2 = second;
-
-	int node1l = 0;
-	
-	int node2l = 0;
+	int node1l = 0, node2l = 0;
 
 	while (node1->parent != NULL)
 	{
 		node1 = node1->parent;
 		node1l++;
 	}
-
 	while (node2->parent != NULL)
 	{
 		node2 = node2->parent;
 		node2l++;
 	}
-
 	node1 = first;
-
+	node2 = second;
 	while (node1l < node2l)
 	{
-		node1 = node1->parent;
-		node1l++;
+		node2 = node2->parent;
+		node2l--;
 	}
-
-	node2 = second;
-
 	while (node2l < node1l)
 	{
-		node2 = node2->parent;
-		node2l++;
+		node1 = node1->parent;
+		node1l--;
 	}
-
 	while (node1 != NULL && node2 != NULL)
 	{
-    		if (node1 == node2)
-        		return ((binary_tree_t *)node1);
-
-    		if (node1l > node2l)
-    		{
-        		node1 = node1->parent;
-        		node1l--;
-    		}
-    		else if (node2l > node1l)
-    		{
-        		node2 = node2->parent;
-        		node2l--;
-    		}
-    		else
-    		{
-        		node1 = node1->parent;
-        		node2 = node2->parent;
-        		node1l--;
-       			node2l--;
-    		}
-}
-
-    	if (node1 == NULL || node2 == NULL)
-	{
-
-        	return (NULL);
+		if (node1 == node2)
+		{
+			return ((binary_tree_t *)node1);
+		}
+		node1 = node1->parent;
+		node2 = node2->parent;
 	}
-
-
 	return ((binary_tree_t *)node1);
 }
