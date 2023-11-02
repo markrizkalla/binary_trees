bd5cc3058374113a189dc47c25f539f40a9f3263
markrizkalla <mark.rizkalla1@gmail.com>
2023-11-02 11:50:17 -0700

    104-binary_tree_rotate_right.c

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -10,13 +10,27 @@
 
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
+	binary_tree_t *pivot, *tmp;
+
 	if (tree == NULL || tree->left == NULL)
 		return (NULL);
 
-	binary_tree_t *newRoot = tree->left;
+	pivot = tree->left;
+	tmp = pivot->right;
+	pivot->right = tree;
+	tree->left = tmp;
+	if (tmp != NULL)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+	if (tmp != NULL)
+	{
+		if (tmp->left == tree)
+			tmp->left = pivot;
+		else
+			tmp->right = pivot;
+	}
 
-	tree->left = newRoot->right;
-	newRoot->right = tree;
-
-	return (newRoot);
+	return (pivot);
 }
