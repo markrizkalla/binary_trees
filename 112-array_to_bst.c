7188186ceec1b76cbb996d13210c210a50a50edc
markrizkalla <mark.rizkalla1@gmail.com>
2023-11-05 09:56:05 -0800

    112-bst_array

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -8,38 +8,27 @@
  *
  * Return: A pointer to the root node of the created BST, or NULL on failure.
  */
-
 bst_t *array_to_bst(int *array, size_t size)
 {
-	bst_t *tree;
-	size_t index = 0;
+	bst_t *tree = NULL;
+	size_t index, jndex;
 
 	if (array == NULL)
 		return (NULL);
 
-	tree = malloc(sizeof(bst_t));
-	tree->n = array[index++];
-
-	while (index < size)
+	for (index = 0; index < size; index++)
 	{
-		int val = array[index];
-		bst_t *curr = tree;
-
-		while (curr != NULL)
+		for (jndex = 0; jndex < index; jndex++)
 		{
-			bst_t **next = (val < curr->n) ? &curr->left : &curr->right;
-
-			if (*next)
-				curr = *next;
-			else
-			{
-				*next = malloc(sizeof(bst_t));
-				(*next)->n = val;
-				(*next)->parent = curr;
+			if (array[jndex] == array[index])
 				break;
-			}
 		}
-		index++;
+		if (jndex == index)
+		{
+			if (bst_insert(&tree, array[index]) == NULL)
+				return (NULL);
+		}
 	}
+
 	return (tree);
 }
