2cffa31b482491fec76595fa96b531a42efa3f3d
markrizkalla <mark.rizkalla1@gmail.com>
2023-11-27 12:31:00 -0800

    avl

--- binary_trees.h
+++ binary_trees.h
@@ -59,4 +59,6 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 
+int binary_tree_is_avl(const binary_tree_t *tree);
+
 #endif /* _BINARY_TREES_H_ */
